a91dec825b55f012bafa23e4aba8868955e0a981
求新 <huliang.hu@alibaba-inc.com>
2022-04-02 13:50:54 +0800

    support 2022

--- mask2former_video/data_video/datasets/builtin.py
+++ mask2former_video/data_video/datasets/builtin.py
@@ -11,23 +11,24 @@ from .ytvis import (
 
 # ==== Predefined splits for YTVIS 2019 ===========
 _PREDEFINED_SPLITS_YTVIS_2019 = {
-    "ytvis_2019_train": ("ytvis_2019/train/JPEGImages",
-                         "ytvis_2019/train.json"),
-    "ytvis_2019_val": ("ytvis_2019/valid/JPEGImages",
-                       "ytvis_2019/valid.json"),
-    "ytvis_2019_test": ("ytvis_2019/test/JPEGImages",
-                        "ytvis_2019/test.json"),
+    "ytvis_2019_train": ("ytvis_2019/train/JPEGImages", "ytvis_2019/train.json"),
+    "ytvis_2019_val": ("ytvis_2019/valid/JPEGImages", "ytvis_2019/valid.json"),
+    "ytvis_2019_test": ("ytvis_2019/test/JPEGImages", "ytvis_2019/test.json"),
 }
 
 
 # ==== Predefined splits for YTVIS 2021 ===========
 _PREDEFINED_SPLITS_YTVIS_2021 = {
-    "ytvis_2021_train": ("ytvis_2021/train/JPEGImages",
-                         "ytvis_2021/train.json"),
-    "ytvis_2021_val": ("ytvis_2021/valid/JPEGImages",
-                       "ytvis_2021/valid.json"),
-    "ytvis_2021_test": ("ytvis_2021/test/JPEGImages",
-                        "ytvis_2021/test.json"),
+    "ytvis_2021_train": ("ytvis_2021/train/JPEGImages", "ytvis_2021/train.json"),
+    "ytvis_2021_val": ("ytvis_2021/valid/JPEGImages", "ytvis_2021/valid.json"),
+    "ytvis_2021_test": ("ytvis_2021/test/JPEGImages", "ytvis_2021/test.json"),
+}
+
+# ==== Predefined splits for YTVIS 2022 ===========
+_PREDEFINED_SPLITS_YTVIS_2022 = {
+    "ytvis_2022_train": ("ytvis_2022/train/JPEGImages", "ytvis_2022/train.json"),
+    "ytvis_2022_val": ("ytvis_2022/valid/JPEGImages", "ytvis_2022/valid.json"),
+    "ytvis_2022_test": ("ytvis_2022/test/JPEGImages", "ytvis_2022/test.json"),
 }
 
 
@@ -53,8 +54,20 @@ def register_all_ytvis_2021(root):
         )
 
 
+def register_all_ytvis_2022(root):
+    for key, (image_root, json_file) in _PREDEFINED_SPLITS_YTVIS_2022.items():
+        # Assume pre-defined datasets live in `./datasets`.
+        register_ytvis_instances(
+            key,
+            _get_ytvis_2021_instances_meta(),
+            os.path.join(root, json_file) if "://" not in json_file else json_file,
+            os.path.join(root, image_root),
+        )
+
+
 if __name__.endswith(".builtin"):
     # Assume pre-defined datasets live in `./datasets`.
     _root = os.getenv("DETECTRON2_DATASETS", "datasets")
     register_all_ytvis_2019(_root)
     register_all_ytvis_2021(_root)
+    register_all_ytvis_2022(_root)
